e81bcef61f7ba27ce2363a41fe42c5751fda5716
Piyushbhatt7 <vengersmon003@gmail.com>
2025-04-27 22:01:45 +0530

    java

--- src/LibraryManagementSystem.java
+++ src/LibraryManagementSystem.java
@@ -7,7 +7,7 @@ public class LibraryManagementSystem extends JFrame {
     private JTextArea displayArea;
  
     Connection con;              
-    PreparedStatement pst;                           
+    PreparedStatement pst;                             
     ResultSet rs; 
  
     public LibraryManagementSystem() {  
@@ -37,7 +37,7 @@ public class LibraryManagementSystem extends JFrame {
         displayArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(displayArea);
 
-        add(idLabel); add(idField);
+        add(idLabel); add(idField); 
         add(titleLabel); add(titleField);
         add(authorLabel); add(authorField);
         add(yearLabel); add(yearField);
